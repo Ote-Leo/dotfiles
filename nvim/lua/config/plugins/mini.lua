f055583b1e04f852e1f0416e9683a88ed0a6fa15
Ote Leo <ote.leo73@gmail.com>
2025-02-09 01:58:29 +0300

    Remove single quote auto-pairing from rust buffers

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -1,19 +1,29 @@
+local function init()
+	local statusline = require "mini.statusline"
+	statusline.setup {
+		use_icons = true,
+	}
+
+	require("mini.ai").setup {}
+	require("mini.jump").setup {}
+	require("mini.jump2d").setup {}
+	require("mini.move").setup {}
+	require("mini.surround").setup {}
+	require("mini.pairs").setup {}
+
+	vim.api.nvim_create_autocmd({ "FileType" }, {
+		pattern = "rust",
+		group = vim.api.nvim_create_augroup("__silver.rust_disable_single_quote_pairs", { clear = true }),
+		desc = "Disabling single quote auto-pairing for rust",
+		callback = function()
+			vim.keymap.set("i", "'", "'", { buffer = true })
+		end
+	})
+end
+
 return {
 	{
 		"echasnovski/mini.nvim",
-		config = function()
-			local statusline = require "mini.statusline"
-
-			statusline.setup {
-				use_icons = true,
-			}
-
-			require("mini.ai").setup()
-			require("mini.jump").setup()
-			require("mini.jump2d").setup()
-			require("mini.move").setup()
-			require("mini.pairs").setup()
-			require("mini.surround").setup()
-		end,
+		init = init,
 	},
 }
