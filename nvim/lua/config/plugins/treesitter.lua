5dc7b3cc3a902dacff7f8e765a29f1e3b7af22da
Ote Leo <ote.leo73@gmail.com>
2025-03-02 13:54:01 +0300

    Adding nvim-treesitter-context for sticky scrolls

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -53,4 +53,28 @@ return {
 		build = ":TSUpdate",
 		config = config,
 	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		init = function()
+			require("treesitter-context").setup {
+				enable = true,
+				multiwindow = false,
+				max_lines = 0,
+				min_window_height = 0,
+				line_numbers = true,
+				multiline_threshold = 20,
+				trim_scope = 'outer',
+				mode = 'cursor',
+				separator = nil,
+				zindex = 20,
+				on_attach = nil,
+			}
+			vim.keymap.set(
+				"n",
+				"[C",
+				function() require("treesitter-context").go_to_context(vim.v.count1) end,
+				{ silent = true, desc = "Jump to current [C]ontext" }
+			)
+		end,
+	}
 }
